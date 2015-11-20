b50790e2de676a23acdb1a8f0b14cacbb815d92c
momyles <maogd_0916@163.com>
2015-11-20 13:08:20 +0800

    1.移除数据库配置工具类jdbcConfig 2.修改数据库配置加载方式,JFinal plugin配置中使用PropKit加载配置文件

--- src/main/java/cn/momyles/ltfz/config/MyJFinalConfig.java
+++ src/main/java/cn/momyles/ltfz/config/MyJFinalConfig.java
@@ -6,12 +6,13 @@ import com.jfinal.config.Interceptors;
 import com.jfinal.config.JFinalConfig;
 import com.jfinal.config.Plugins;
 import com.jfinal.config.Routes;
+import com.jfinal.kit.Prop;
+import com.jfinal.kit.PropKit;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.c3p0.C3p0Plugin;
 
 import cn.momyles.ltfz.controller.LoginController;
 import cn.momyles.ltfz.pojo.User;
-import cn.momyles.ltfz.util.DBConfigUtil;
 /**
  * My JFinal Config Class
  * @author MoMyles
@@ -26,19 +27,23 @@ public class MyJFinalConfig extends JFinalConfig {
 	
 	@Override
 	public void configRoute(Routes me) {
-		me.add("/login", LoginController.class);
+		me.add("/login", LoginController.class, "/");
 	}
 	
 	@Override
 	public void configPlugin(Plugins me) {
-		C3p0Plugin cp = new C3p0Plugin(DBConfigUtil.getPropertiesFile());
-		me.add(cp);
+		// 读取数据库配置文件
+		Prop prop = PropKit.use("jdbcConfig.properties");
+		// 将配置参数传入C3p0
+		C3p0Plugin cp = new C3p0Plugin(prop.get("jdbcUrl"), prop.get("user"), prop.get("password")
+				, prop.get("driverClass"), prop.getInt("maxPoolSize"),prop.getInt("minPoolSize")
+				, prop.getInt("initialPoolSize"), prop.getInt("maxIdleTime")
+				, prop.getInt("acquireIncrement"));
+		me.add(cp);// 添加c3p0插件
 		ActiveRecordPlugin arp = new ActiveRecordPlugin(cp);
 		me.add(arp);
 		arp.addMapping("user", User.class);
 	}
-
-
 
 	@Override
 	public void configHandler(Handlers me) {
--- src/main/java/cn/momyles/ltfz/controller/LoginController.java
+++ src/main/java/cn/momyles/ltfz/controller/LoginController.java
@@ -1,5 +1,8 @@
 package cn.momyles.ltfz.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,7 +12,15 @@ public class LoginController extends Controller {
 	// 记录日志信息
 	private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 	public void index() {
-		log.warn(new String("Hello SomeBody My Log4j!"));
-		renderText("Hello My First JFinal!");
+		setAttr("name", null);
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		setAttr("list", list);
+		renderFreeMarker("index.flt");
 	}
 }
